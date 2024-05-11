c24917566d169daf17b0069e1726859258fcb3ef
Владимир Толстов <vlatolstov@gmail.com>
2024-05-11 19:01:34 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LeetCodeDaily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public class Solution
+        {
+
+
+        }
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static TreeNode CreateTree(int?[] values, int index = 0)
+        {
+            if (index >= values.Length || values[index] == null)
+            {
+                return null;
+            }
+            TreeNode node = new TreeNode();
+            node.left = CreateTree(values, 2 * index + 1);
+            node.right = CreateTree(values, 2 * index + 2);
+            node.val = (int)values[index];
+            return node;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
