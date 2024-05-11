ce5d534047f4c0bf69349e9a554cca5780fe0d11
Владимир Толстов <vlatolstov@gmail.com>
2024-05-11 19:22:06 +0400

    Time comp.: O(n log n) Space comp.: O(n)

--- Program.cs
+++ Program.cs
@@ -9,49 +9,39 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public double MincostToHireWorkers(int[] quality, int[] wage, int k)
             {
-                return null;
+                int n = quality.Length;
+                List<(int, double)> ratios = new();
+
+                for (int i = 0; i < n; i++)
+                    ratios.Add((quality[i], (double)wage[i] / quality[i]));
+
+                ratios = ratios.OrderBy(x => x.Item2).ToList();
+
+                int totalQuality = 0;
+                double res = 0;
+                PriorityQueue<int, int> q = new();
+
+                for (int i = 0; i < n; i++)
+                {
+                    q.Enqueue(ratios[i].Item1, -1 * ratios[i].Item1);
+                    totalQuality += ratios[i].Item1;
+
+                    if (i + 1 >= k)
+                    {
+                        res = res == 0 ? totalQuality * ratios[i].Item2 : Math.Min(res, totalQuality * ratios[i].Item2);
+                        totalQuality -= q.Dequeue();
+                    }
+                }
+
+                return res;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
+
         }
     }
 }
