6dbc435a75e6442df2ad433dd245fa906004d0df
Владимир Толстов <vlatolstov@gmail.com>
2024-05-12 23:46:53 +0400

    Time complexity: O(n^2) Space complexity:O(n^2)

--- Program.cs
+++ Program.cs
@@ -9,49 +9,45 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            var sol = new Solution();
+
+            int[][] a = [[9, 9, 8, 1], [5, 6, 2, 6], [8, 2, 6, 4], [6, 2, 2, 2]];
+            int[][] b = [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 2, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]];
+
+            sol.LargestLocal(a);
+            sol.LargestLocal(b);
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int[][] LargestLocal(int[][] grid)
             {
-                return null;
+                int n = grid.Length;
+                var res = new int[n - 2][];
+                for (int i = 0; i < n - 2; i++)
+                {
+                    res[i] = new int[n - 2];
+                    for (int j = 0; j < n - 2; j++)
+                    {
+                        res[i][j] = Recursive(grid, i + 1, j + 1);
+                    }
+                }
+
+                return res;
+
+                int Recursive(int[][] grid, int i, int j)
+                {
+                    int max = 0;
+                    for (int raw = i - 1; raw <= i + 1; raw++)
+                    {
+                        for (int col = j - 1; col <= j + 1; col++)
+                        {
+                            max = Math.Max(max, grid[raw][col]);
+                        }
+                    }
+                    return max;
+                }
+
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
