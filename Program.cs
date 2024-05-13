67fa4c1a885b8be50e9caf09e1aa5d0e44bf0d7f
Владимир Толстов <vlatolstov@gmail.com>
2024-05-13 12:11:45 +0400

    T:O(m*n) S:O(1)

--- Program.cs
+++ Program.cs
@@ -9,49 +9,47 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            var sol = new Solution();
+            int[][] a = [[0, 0, 1, 1], [1, 0, 1, 0], [1, 1, 0, 0]];
+            Console.WriteLine(sol.MatrixScore(a));
+            int[][] b = [[0]];
+            Console.WriteLine(sol.MatrixScore(b));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int MatrixScore(int[][] grid)
             {
-                return null;
+                int m = grid.Length;
+                int n = grid[0].Length;
+
+                for (int i = 0; i < m; i++) if (grid[i][0] == 0) FlipRaw(grid, i);
+
+                for (int j = 0; j < n; j++)
+                {
+                    int ratio = 0;
+                    for (int i = 0; i < m; i++) ratio += grid[i][j] == 0 ? -1 : 1;
+                    if (ratio < 0) FlipCol(grid, j);
+                }
+
+                int sum = 0;
+                foreach(var raw  in grid) sum += Convert.ToInt32(String.Concat(raw), 2);
+                return sum;
+
+                void FlipRaw(int[][] grid, int i)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        grid[i][j] = grid[i][j] == 0 ? 1 : 0;
+                    }
+                }
+                void FlipCol(int[][] grid, int j)
+                {
+                    for (int i = 0; i < m; i++)
+                    {
+                        grid[i][j] = grid[i][j] == 0 ? 1 : 0;
+                    }
+                }
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
