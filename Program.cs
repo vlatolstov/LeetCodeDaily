cfcb2d39618b68e3aec118c7bac8f39c9505ee1e
Владимир Толстов <vlatolstov@gmail.com>
2024-05-18 19:14:50 +0400

    0

--- Program.cs
+++ Program.cs
@@ -13,8 +13,22 @@ namespace LeetCodeDaily
         }
         public class Solution
         {
+            public int DistributeCoins(TreeNode root)
+            {
+                int steps = 0;
+                Rec(root);
+                return steps;
 
+                int Rec(TreeNode node)
+                {
+                    int coins = node.val;
 
+                    if (node.left != null) coins += Rec(node.left);
+                    if (node.right != null) coins += Rec(node.right);
+                    steps += Math.Abs(coins - 1);
+                    return coins - 1;
+                }
+            }
         }
     }
 
@@ -42,16 +56,6 @@ namespace LeetCodeDaily
             node.right = CreateTree(values, 2 * index + 2);
             node.val = (int)values[index];
             return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
