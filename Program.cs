e77e1c852c1407f28acc869623d9af8ccd926791
Владимир Толстов <vlatolstov@gmail.com>
2024-05-21 08:46:28 +0400

    0

--- Program.cs
+++ Program.cs
@@ -9,49 +9,41 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            int[] a = [1, 2, 3];
+            sol.Subsets(a);
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public IList<IList<int>> Subsets(int[] nums)
             {
-                return null;
+                Queue<(IList<int>, int)> q = [];
+                HashSet<IList<int>> res = [];
+                q.Enqueue((nums.ToList(), 0));
+                
+                while (q.Count > 0)
+                {
+                    int n = q.Count;
+                    for (int i = 0; i < n; i++)
+                    {
+                        var cur = q.Dequeue();
+                        var subset = cur.Item1;
+                        var index = cur.Item2;
+                        res.Add(subset);
+                        if (index < subset.Count)
+                        {
+                            IList<int> newSubset = [];
+                            for (int j = 0; j < subset.Count; j++)
+                            {
+                                if (j != index) newSubset.Add(subset[j]);
+                            }
+                            q.Enqueue((subset, index + 1));
+                            q.Enqueue((newSubset, 0));
+                        }
+                    }
+                }
+                return [.. res];
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
