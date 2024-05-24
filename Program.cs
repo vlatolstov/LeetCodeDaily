789fdba2dd90a58ec8ebc4d05b134aa2ffc185ce
Владимир Толстов <vlatolstov@gmail.com>
2024-05-24 12:30:21 +0400

    -

--- Program.cs
+++ Program.cs
@@ -9,49 +9,38 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            int[] a = [3, 2, 3];
+            Console.WriteLine(sol.MajorityElement(a));
+
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int MajorityElement(int[] nums)
             {
-                return null;
+                Array.Sort(nums);
+                int elem = nums[0];
+                int cur = 1;
+                if (nums.Length == 1) return elem;
+
+                (int, int) max = (elem, cur);
+
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] == elem)
+                    {
+                        cur++;
+                    }
+                    else
+                    {
+                        max = max.Item2 > cur ? max : (elem, cur);
+                        elem = nums[i];
+                        cur = 1;
+                    }
+                }
+                max = max.Item2 > cur ? max : (elem, cur);
+                return max.Item1;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
