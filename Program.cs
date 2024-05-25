b76632102669f40c2a7c45f62ed9fad718672f71
Владимир Толстов <vlatolstov@gmail.com>
2024-05-25 14:20:46 +0400

    1

--- Program.cs
+++ Program.cs
@@ -9,49 +9,43 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            string s1 = "catsanddog";
+            List<string> dict1 = ["cat", "cats", "and", "sand", "dog"];
+            foreach (string s in sol.WordBreak(s1, dict1)) Console.WriteLine(s);
+            string s2 = "a";
+            List<string> dict2 = ["a"];
+            foreach (string s in sol.WordBreak(s2, dict2)) Console.WriteLine(s);
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public IList<string> WordBreak(string s, IList<string> wordDict)
             {
-                return null;
+                HashSet<string> valid = new HashSet<string>(wordDict);
+                IList<string> result = new List<string>();
+                BFS (0, 1, new List<string>());
+                return result;
+
+                void BFS(int l, int r, IList<string> sentence)
+                {
+                    if (r == s.Length)
+                    {
+                        result.Add(String.Join(" ", sentence));
+                        return;
+                    }
+                    while (r <= s.Length)
+                    {
+                        var cur = s.Substring(l, r - l);
+                        if (valid.Contains(cur))
+                        {
+                            sentence.Add(cur);
+                            BFS (r, r, new List<string>(sentence));
+                            sentence.RemoveAt(sentence.Count - 1);
+                        }
+                        r++;
+                    }
+                }
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
