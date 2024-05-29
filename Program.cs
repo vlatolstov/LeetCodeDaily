ff1c9a8fea5e18865fb032f5ac826e4fc7bd87d5
Владимир Толстов <vlatolstov@gmail.com>
2024-05-29 21:58:21 +0400

    -

--- Program.cs
+++ Program.cs
@@ -12,36 +12,47 @@ namespace LeetCodeDaily
             Solution sol = new();
             int[] a = [7, 1, 5, 3, 6, 4];
             Console.WriteLine(sol.MaxProfit(a));
-            var b = Enumerable.Range(1, 1000).ToArray();
-            Console.WriteLine(sol.MaxProfit(b));
+            //var b = Enumerable.Range(1, 1000).ToArray();
+            //Console.WriteLine(sol.MaxProfit(b));
         }
         public class Solution
         {
             public int MaxProfit(int[] prices)
             {
+                Dictionary<int, List<(int, int)>> graph = [];
+
+                for (int i = 0; i < prices.Length; i++)
+                {
+                    graph.Add(i, []);
+                    for (int j = i + 1; j < prices.Length; j++)
+                    {
+                        int profit = prices[j] - prices[i];
+                        if (profit > 0) graph[i].Add((j, profit));
+                    }
+                }
                 int max = 0;
-                DFS(0, 0);
+                BFS(0, 0);
+
                 return max;
 
-                void DFS (int l, int sum)
+                void BFS(int pos, int sum)
                 {
-                    if (l >= prices.Length)
+                    if (pos >= graph.Count)
                     {
                         max = Math.Max(max, sum);
                         return;
                     }
 
-                    for (int r = l; r < prices.Length; r++)
+                    for (int i = pos; i < graph.Count; i++)
                     {
-                        int profit = prices[r] - prices[l];
-                        if (profit > 0)
+                        foreach (var kvp in graph[i])
                         {
-                            sum += profit;
-                            DFS(r + 1, sum);
-                            sum -= profit;
+                            sum += kvp.Item2;
+                            BFS(kvp.Item1 + 1, sum);
+                            sum -= kvp.Item2;
                         }
                     }
-                    DFS(l + 1, sum);
+                   
                 }
             }
         }
