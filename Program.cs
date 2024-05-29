92729f68b8e4e4379857bfd85eddbca0a9129e5a
Владимир Толстов <vlatolstov@gmail.com>
2024-05-29 21:34:57 +0400

    no

--- Program.cs
+++ Program.cs
@@ -9,49 +9,41 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            int[] a = [7, 1, 5, 3, 6, 4];
+            Console.WriteLine(sol.MaxProfit(a));
+            var b = Enumerable.Range(1, 1000).ToArray();
+            Console.WriteLine(sol.MaxProfit(b));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int MaxProfit(int[] prices)
             {
-                return null;
+                int max = 0;
+                DFS(0, 0);
+                return max;
+
+                void DFS (int l, int sum)
+                {
+                    if (l >= prices.Length)
+                    {
+                        max = Math.Max(max, sum);
+                        return;
+                    }
+
+                    for (int r = l; r < prices.Length; r++)
+                    {
+                        int profit = prices[r] - prices[l];
+                        if (profit > 0)
+                        {
+                            sum += profit;
+                            DFS(r + 1, sum);
+                            sum -= profit;
+                        }
+                    }
+                    DFS(l + 1, sum);
+                }
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
