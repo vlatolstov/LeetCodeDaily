b1b314d400c12a030ba78468a065154cfe195317
Владимир Толстов <vlatolstov@gmail.com>
2024-05-29 14:28:25 +0400

    1

--- Program.cs
+++ Program.cs
@@ -10,43 +10,54 @@ namespace LeetCodeDaily
         static void Main(string[] args)
         {
             Solution sol = new();
-            string a = "1";
+            string a = "1101";
             Console.WriteLine(sol.NumSteps(a));
+            string b = "10";
+            Console.WriteLine(sol.NumSteps(b));
+            string c = "1";
+            Console.WriteLine(sol.NumSteps(c));
+            string d = "1000000000000010000100100000100010000000101011101101000101";
+            Console.WriteLine(sol.NumSteps(d));
+            string e = "1111110011101010110011100100101110010100101110111010111110110010";
+            Console.WriteLine(sol.NumSteps(e));
         }
         public class Solution
         {
             public int NumSteps(string s)
             {
-                long x = ToDecimal(s);
+                int i = s.Length - 1;
+                int transpose = 0;
                 int steps = 0;
 
-                while (x != 1)
+                while (i > 0)
                 {
-                    if (x % 2 == 0)
+                    char bit = s[i];
+
+                    switch (bit, transpose > 0)
                     {
-                        x /= 2;
+                        case ('1', true):
+                            bit = '0';
+                            break;
+                        case ('0', true):
+                            bit = '1';
+                            transpose--;
+                            break;
+                        default:
+                            break;
                     }
+
+                    if (bit == '0') i--;
                     else
                     {
-                        x += 1;
+                        transpose++;
+                        steps++;
+                        i--;
                     }
+
                     steps++;
                 }
-                return steps;
 
-                long ToDecimal(string binary)
-                {
-                    long val = 0;
-                    int length = binary.Length;
-
-                    for (int i = 0; i < length; i++)
-                    {
-                        char bit = binary[length - 1 - i];
-                        if (bit == '1') val += (long)Math.Pow(2, i);
-                    }
-
-                    return val;
-                }
+                return steps + transpose;
             }
         }
     }
