b7ffdffbfbd8d28aca28560cd17d36eb39a0b8b8
Владимир Толстов <vlatolstov@gmail.com>
2024-06-02 14:47:17 +0400

    0

--- Program.cs
+++ Program.cs
@@ -9,49 +9,23 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public void ReverseString(char[] s)
             {
-                return null;
+                int l = 0, r = s.Length - 1;
+
+                while (l < r)
+                {
+                    s[l] ^= s[r];
+                    s[r] ^= s[l];
+                    s[l] ^= s[r];
+                    l++;
+                    r--;
+                }
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
