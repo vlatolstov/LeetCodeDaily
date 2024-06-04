ffb29cb59f2c200fd64fcfcb897e0df589192c75
Владимир Толстов <vlatolstov@gmail.com>
2024-06-04 09:25:36 +0400

    -

--- Program.cs
+++ Program.cs
@@ -9,49 +9,33 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            string a = "aabbddbbssbbfbsbdAAAASssSFKDNJJJJFDnnnDNbNDNNNNNNNNNNNNnnnnnnnnnn";
+            Console.WriteLine(sol.LongestPalindrome(a));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int LongestPalindrome(string s)
             {
-                return null;
+                Dictionary<char, int> d = [];
+
+                for (int i = 0; i < s.Length; i++) if (!d.TryAdd(s[i], 1)) d[s[i]]++;
+
+                int length = 0;
+                bool odd = false;
+
+                foreach (int val in d.Values)
+                {
+                    if (val % 2 == 0) length += val;
+                    else
+                    {
+                        odd = true;
+                        length += val - 1;
+                    }
+                }
+
+                return odd ? length + 1 : length;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
