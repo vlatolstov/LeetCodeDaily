6e9632ba4463c2e4cabaece078108c01782a8176
Владимир Толстов <vlatolstov@gmail.com>
2024-06-06 17:29:37 +0400

    -

--- Program.cs
+++ Program.cs
@@ -13,45 +13,11 @@ namespace LeetCodeDaily
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public bool IsNStraightHand(int[] hand, int groupSize)
             {
-                return null;
+                if (hand.Length % groupSize != 0) return false;
+
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
