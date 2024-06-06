bda1f2afa8ad550c68f696709f42609dd871cac5
Владимир Толстов <vlatolstov@gmail.com>
2024-06-06 17:41:11 +0400

    -

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            sol.IsNStraightHand([1, 2, 3, 6, 2, 3, 4, 7, 8], 3);
         }
         public class Solution
         {
@@ -17,6 +18,46 @@ namespace LeetCodeDaily
             {
                 if (hand.Length % groupSize != 0) return false;
 
+                RadixSort(hand);
+                return true;
+            }
+            private const int NUM_DIGITS = 10;
+            void RadixSort(int[] arr)
+            {
+                int max = int.MinValue;
+                foreach (int i in arr) max = Math.Max(max, i);
+
+                int place = 1;
+                while (max / place > 0)
+                {
+                    CountingSort(arr, place);
+                    place *= 10;
+                }
+            }
+
+            void CountingSort(int[] arr, int place)
+            {
+                var counts = new int[NUM_DIGITS];
+
+                foreach (int i in arr) counts[i / place % NUM_DIGITS]++;
+
+                int startIndex = 0;
+                for (int i = 0; i < counts.Length; i++)
+                {
+                    int count = counts[i];
+                    counts[i] = startIndex;
+                    startIndex += count;
+                }
+
+                var sorted = new int[arr.Length];
+                foreach (int i in arr)
+                {
+                    int cur = i / place;
+                    sorted[counts[cur % NUM_DIGITS]] = i;
+                    counts[cur % NUM_DIGITS]++;
+                }
+
+                for (int i = 0; i < arr.Length; i++) arr[i] = sorted[i];
             }
         }
     }
