e9f753fec3b67619018d12b4661cf26475c71912
Владимир Толстов <vlatolstov@gmail.com>
2024-06-09 21:46:45 +0400

    0

--- Program.cs
+++ Program.cs
@@ -9,49 +9,31 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            int[] a = [4, 5, 0, -2, -3, 1];
+            Console.WriteLine(sol.SubarraysDivByK(a, 5));
+            int[] b = [-1, 2, 9];
+            Console.WriteLine(sol.SubarraysDivByK(b, 2));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int SubarraysDivByK(int[] nums, int k)
             {
-                return null;
+                Dictionary<int, int> prefixSum = []; //key is modulo of current sum, val is count
+                prefixSum[0] = 1;
+                int count = 0;
+                int sum = 0;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    sum += nums[i];
+                    int mod = sum % k;
+                    if (mod < 0) mod += k;
+                    if (prefixSum.TryGetValue(mod, out int value)) count += value;
+                    if (!prefixSum.TryAdd(mod, 1)) prefixSum[mod]++;
+                }
+                return count;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
