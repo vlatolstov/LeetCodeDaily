aacfcd7d01a992d20a98278a5e0358d880512ce9
Владимир Толстов <vlatolstov@gmail.com>
2024-06-17 14:07:06 +0400

    1

--- Program.cs
+++ Program.cs
@@ -9,49 +9,23 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            Console.WriteLine(sol.JudgeSquareSum(2));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public bool JudgeSquareSum(int c)
             {
-                return null;
+                int a = 0;
+                while (a * a <= c)
+                {
+                    var bSqr = c - a * a; 
+                    var b = Math.Sqrt(bSqr);
+                    if (b % 1 == 0) return true;
+                    a++;
+                }
+                return false;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
