d54a67d68014a153a9d39920ee508452ac07963c
Владимир Толстов <vlatolstov@gmail.com>
2024-06-21 16:45:56 +0400

    0

--- Program.cs
+++ Program.cs
@@ -9,49 +9,41 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            Console.WriteLine(sol.MaxSatisfied([4, 10, 10], [1, 1, 0], 2));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int MaxSatisfied(int[] customers, int[] grumpy, int minutes)
             {
-                return null;
+                int sum = 0;
+                for (int i = 0; i < minutes; i++)
+                {
+                    sum += customers[i];
+                }
+
+                int left = 0, right = minutes - 1;
+                (int, int, int) bestWindow = (sum, left, right);
+
+                while (right < customers.Length - 1)
+                {
+                    sum += customers[++right] - customers[left++];
+                    if (sum > bestWindow.Item1) bestWindow = (sum, left, right);
+                }
+
+                sum = bestWindow.Item1;
+
+                for (int i = 0; i < bestWindow.Item2; i++)
+                {
+                    if (grumpy[i] == 0) sum += customers[i];
+                }
+                for (int i = bestWindow.Item3; i < customers.Length - 1; i++)
+                {
+                    if (grumpy[i] == 0) sum += customers[i];
+                }
+
+                return sum;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
