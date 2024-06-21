bdc63495d7f5020bac823daa6a1795777703fd2c
Владимир Толстов <vlatolstov@gmail.com>
2024-06-21 16:51:58 +0400

    1

--- Program.cs
+++ Program.cs
@@ -17,32 +17,28 @@ namespace LeetCodeDaily
             public int MaxSatisfied(int[] customers, int[] grumpy, int minutes)
             {
                 int sum = 0;
+                for (int i = 0; i < customers.Length; i++)
+                {
+                    if (grumpy[i] == 0) sum += customers[i];
+                }
+
                 for (int i = 0; i < minutes; i++)
                 {
-                    sum += customers[i];
+                    if (grumpy[i] == 1) sum += customers[i];
                 }
 
-                int left = 0, right = minutes - 1;
-                (int, int, int) bestWindow = (sum, left, right);
+                int maxSum = sum, left = 0, right = minutes;
 
-                while (right < customers.Length - 1)
+                while (right < customers.Length)
                 {
-                    sum += customers[++right] - customers[left++];
-                    if (sum > bestWindow.Item1) bestWindow = (sum, left, right);
+                    if (grumpy[left] == 1) sum -= customers[left];
+                    if (grumpy[right] == 1) sum += customers[right];
+                    maxSum = Math.Max(maxSum, sum);
+                    left++;
+                    right++;
                 }
 
-                sum = bestWindow.Item1;
-
-                for (int i = 0; i < bestWindow.Item2; i++)
-                {
-                    if (grumpy[i] == 0) sum += customers[i];
-                }
-                for (int i = bestWindow.Item3; i < customers.Length - 1; i++)
-                {
-                    if (grumpy[i] == 0) sum += customers[i];
-                }
-
-                return sum;
+                return maxSum;
             }
         }
     }
