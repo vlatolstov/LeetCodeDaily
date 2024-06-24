42baf78caa609f110b95dea009b3c1f0e98ff4d3
Владимир Толстов <vlatolstov@gmail.com>
2024-06-24 18:08:50 +0400

    -

--- Program.cs
+++ Program.cs
@@ -9,49 +9,46 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            int[][] a = [[2, 1, 1], [1, 1, 0], [0, 1, 1]];
+            Console.WriteLine(sol.OrangesRotting(a));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int OrangesRotting(int[][] grid)
             {
-                return null;
+                int[][] dir = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+                int curRotten = 2;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int raw = 0; raw < grid.Length; raw++)
+                    {
+                        for (int col = 0; col < grid[raw].Length; col++)
+                        {
+                            if (grid[raw][col] == curRotten)
+                            {
+                                foreach (var d in dir)
+                                {
+                                    if (raw > 0 && raw < grid.Length - 1 &&
+                                        col > 0 && col < grid[raw].Length - 1 &&
+                                        grid[raw + d[0]][col + d[1]] == 1)
+                                    {
+                                        grid[raw + d[0]][col + d[1]] = curRotten + 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    curRotten++;
+                }
+
+                for (int raw = 0; raw <= grid.Length; raw++)
+                    for (int col = 0; col <= grid[raw].Length; col++)
+                        if (grid[raw][col] == 1) return -1;
+
+                return curRotten + 1;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
         }
     }
 }
