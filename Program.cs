6924dafaa175ec8ee43fd2a17aa3407375f92a9f
Владимир Толстов <vlatolstov@gmail.com>
2024-07-16 19:17:43 +0400

    1

--- Program.cs
+++ Program.cs
@@ -9,65 +9,30 @@ namespace LeetCodeDaily
     {
         static void Main(string[] args)
         {
-            
+            Solution sol = new();
+            Console.WriteLine(sol.FindJudge(2, [[1, 2]]));
+            Console.WriteLine(sol.FindJudge(3, [[1, 3], [2, 3]]));
+            Console.WriteLine(sol.FindJudge(3, [[1, 3], [2, 3], [3, 1]]));
         }
         public class Solution
         {
-
-
-        }
-    }
-
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
+            public int FindJudge(int n, int[][] trust)
             {
-                return null;
+                var all = new int[n + 1, 2];
+
+                foreach (var arr in trust)
+                {
+                    all[arr[0], 0]++;
+                    all[arr[1], 1]++;
+                }
+
+                for (int i = 1; i < n + 1; i++)
+                {
+                    if (all[i, 0] == 0 && all[i, 1] == n - 1) return i;
+                }
+
+                return -1;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-
-        public static ListNode CreateLinkedList(int[] vals)
-        {
-            if (vals.Length == 0) return null;
-
-            ListNode head = new(vals[0]);
-            ListNode cur = head;
-
-            for (int i = 1; i < vals.Length; i++)
-            {
-                cur.next = new(vals[i]);
-                cur = cur.next;
-            }
-
-            return head;
         }
     }
 }
