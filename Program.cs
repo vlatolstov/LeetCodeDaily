0736904aa04382c693cb2181d38f0b075af39df7
Владимир Толстов <vlatolstov@gmail.com>
2024-10-09 11:25:17 +0400

    -

--- Program.cs
+++ Program.cs
@@ -3,70 +3,34 @@ using System.Text;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace LeetCodeDaily
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-        public class Solution
-        {
+namespace LeetCodeDaily {
+    internal class Program {
+        static void Main(string[] args) {
 
         }
-    }
+        public class Solution {
+            public int MinAddToMakeValid(string s) {
 
+                int open = 0;
+                int close = 0;
 
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+                foreach (char c in s) {
 
-        public static TreeNode CreateTree(int?[] values, int index = 0)
-        {
-            if (index >= values.Length || values[index] == null)
-            {
-                return null;
+                    if (c == '(') {
+                        open++;
+                    }
+
+                    else {
+
+                        if (open > 0)
+                            open--;
+                        else
+                            close++;
+                    }
+                }
+
+                return open + close;
             }
-            TreeNode node = new TreeNode();
-            node.left = CreateTree(values, 2 * index + 1);
-            node.right = CreateTree(values, 2 * index + 2);
-            node.val = (int)values[index];
-            return node;
-        }
-    }
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-
-        public static ListNode CreateLinkedList(int[] vals)
-        {
-            if (vals.Length == 0) return null;
-
-            ListNode head = new(vals[0]);
-            ListNode cur = head;
-
-            for (int i = 1; i < vals.Length; i++)
-            {
-                cur.next = new(vals[i]);
-                cur = cur.next;
-            }
-
-            return head;
         }
     }
 }
