50695297b0698706c8ec34d0b0d7f857e258b008
Владимир Толстов <vlatolstov@gmail.com>
2024-10-14 11:01:07 +0400

    -

---
+++ Custom/ListNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDaily.Custom {
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public static ListNode CreateLinkedList(int[] vals) {
+            if (vals.Length == 0)
+                return null;
+
+            ListNode head = new(vals[0]);
+            ListNode cur = head;
+
+            for (int i = 1; i < vals.Length; i++) {
+                cur.next = new(vals[i]);
+                cur = cur.next;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ Custom/TreeNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDaily.Custom {
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static TreeNode CreateTree(int?[] values, int index = 0) {
+            if (index >= values.Length || values[index] == null) {
+                return null;
+            }
+            TreeNode node = new TreeNode();
+            node.left = CreateTree(values, 2 * index + 1);
+            node.right = CreateTree(values, 2 * index + 2);
+            node.val = (int)values[index];
+            return node;
+        }
+    }
+}
