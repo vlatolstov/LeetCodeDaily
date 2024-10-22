c72224de3962ba893f73f3f7077af75d131aa10d
Владимир Толстов <vlatolstov@gmail.com>
2024-10-23 01:22:12 +0400

    -

--- Program.cs
+++ Program.cs
@@ -11,7 +11,35 @@ namespace LeetCodeDaily {
 
         }
         public class Solution {
+            public long KthLargestLevelSum(TreeNode root, int k) {
 
+                List<long> sums = [];
+                Queue<TreeNode> q = [];
+                q.Enqueue(root);
+
+                while (q.Count > 0) {
+                    int size = q.Count;
+                    long sum = 0;
+
+                    for (int i = 0; i < size; i++) {
+                        var curNode = q.Dequeue();
+                        sum += curNode.val;
+
+                        if (curNode.left != null)
+                            q.Enqueue(curNode.left);
+
+                        if (curNode.right != null)
+                            q.Enqueue(curNode.right);
+                    }
+
+                    sums.Add(sum);
+                }
+
+                if (sums.Count < k)
+                    return -1;
+
+                return sums.OrderByDescending(x => x).ElementAt(k - 1);
+            }
         }
     }
 }
