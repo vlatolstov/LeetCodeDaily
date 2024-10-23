4b89f99b16312b71d4c0e49878476f2d9f730941
Владимир Толстов <vlatolstov@gmail.com>
2024-10-23 09:19:35 +0400

    -

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
 using LeetCodeDaily.Custom;
+using System.ComponentModel.Design;
 
 namespace LeetCodeDaily {
     internal class Program {
@@ -11,7 +12,45 @@ namespace LeetCodeDaily {
 
         }
         public class Solution {
+            private List<int> levelSums;
+            public TreeNode ReplaceValueInTree(TreeNode root) {
+                levelSums = [];
 
+                CalculateLevelSums(root, 0);
+
+                ChangeValue(root, 0, 0);
+
+                return root;
+            }
+
+            private void CalculateLevelSums(TreeNode root, int depth) {
+                if (root == null)
+                    return;
+
+                if (depth >= levelSums.Count) {
+                    levelSums.Add(root.val);
+                }
+                else {
+                    levelSums[depth] += root.val;
+                }
+
+                CalculateLevelSums(root.left, depth + 1);
+                CalculateLevelSums(root.right, depth + 1);
+            }
+
+            private void ChangeValue(TreeNode root, int value, int depth) {
+                if (root == null)
+                    return;
+
+                root.val = value;
+
+                int nextValue = depth + 1 < levelSums.Count ? levelSums[depth + 1] : 0;
+                nextValue -= root.left == null ? 0 : root.left.val;
+                nextValue -= root.right == null ? 0 : root.right.val;
+
+                ChangeValue(root.left, nextValue, depth + 1);
+                ChangeValue(root.right, nextValue, depth + 1);
+            }
         }
     }
 }
