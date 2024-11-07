ea351c60632a4c2783961fbc8a192c696588e063
Владимир Толстов <vlatolstov@gmail.com>
2024-11-07 18:13:35 +0400

    -

--- Program.cs
+++ Program.cs
@@ -8,10 +8,31 @@ using LeetCodeDaily.Custom;
 namespace LeetCodeDaily {
     internal class Program {
         static void Main(string[] args) {
-
         }
         public class Solution {
 
+            public int LargestCombination(int[] candidates) {
+                int[] bitCounts = new int[24];
+
+                foreach (int candidate in candidates) {
+                    int num = candidate;
+                    int sign = 0;
+
+                    while (num > 0) {
+                        bitCounts[sign] += num % 2;
+                        num /= 2;
+                        sign++;
+                    }
+                }
+
+                int maxLength = 0;
+
+                foreach (int bits in bitCounts) {
+                    maxLength = Math.Max(maxLength, bits);
+                }
+
+                return maxLength;
+            }
         }
     }
 }
